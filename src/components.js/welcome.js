9de80cc5f405dd0abb483fc4d991db41b9da948b
Laboratoria/SAP010-card-validation <carolvas98@hotmail.com>
2023-06-19 15:02:15 -0300

    teste

--- src/components.js/welcome.js
+++ src/components.js/welcome.js
@@ -4,7 +4,79 @@ export const welcome = () => {
   const container = document.createElement('div');
 
   const signInHTML = `
-    // Código HTML omitido para brevidade
+     <section class="container-parent">
+        <div class="container-img-animals">
+            <img src="../img/animalsBgp.png" class="img-animals">
+              <div class="sobre-petChat">
+                 Conecte-se no <strong>PetChat</strong>, compartilhe dúvidas e experiências sobre seus pets,
+                 faça novas conexões com amantes de animais e receba suporte em cuidados, 
+                 alimentação e treinamento.
+              </div>
+                  <div class="containerWelcome">
+                    <div class="bgContainer"></div>
+                    <div class="divSignIn">
+                      <img src="../img/LogoPetChat.png" class="logoPetWelcome">
+                          <label class="label" for="email">E-mail:</label>
+                            <div class="inputGroup">
+                            <i class="material-icons inputIcon">person_outline</i>
+                                <input type="email" class="inputSignIn" id="email" placeholder="example@youremail.com" required>
+                  </div>
+
+              <label class="label" for="pass">Senha:</label>
+
+              <div class="inputGroup">
+                  <i class="material-icons inputIcon">lock</i>
+                  <input type="password" class="inputSignIn" id="pass" placeholder="*******" required minlength="6">
+              </div>
+
+              <div id="error-container-email">
+              <ul class="error-email-password"></ul>
+              </div>
+
+            <button class="btnSignIn active">Entrar</button>
+
+            <p class="textOu">----------------------------- ou -----------------------------</p>
+
+                <button class="btnGoogle">
+                  <img class="img-google" src="img/google_logo.png" />
+                  Google
+                </button>
+
+                <button class="btnGitHub">
+                  <img class="img-github" src="img/github.logo.png" />
+                  GitHub
+                </button>
+
+                <div id="error-Google-Github">
+                <ul class="error-Google-Github"></ul>
+                </div>
+
+              <p><a class="btnRegister"><i class="material-icons petIcon">pets</i> Criar uma conta </a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+            <footer>
+                <p> <strong> Projeto realizado durante o Bootcamp da Laboratória </strong> </p>
+                    <section class="contato">
+                        <p class="nome-aluna">Carol Protásio</p>
+                        <i class="fab fa-github"></i>
+                          <a href="https://github.com/carolprotasio"  target="_blank">GitHub</a> | <i class="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/carol-prot%C3%A1sio-8b4a34249/" target="_blank">Linkedin</a>
+                    </section>
+
+                    <section class="contato">
+                        <p class="nome-aluna">Caroline Vaz</p>
+                        <i class="fab fa-github"></i>
+                          <a href="https://github.com/carolvaz98"  target="_blank">GitHub</a> | <i class="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/caroline-v-b95019121/" target="_blank">Linkedin</a>
+                    </section>
+
+                    <section class="contato">
+                        <p class="nome-aluna">Myllena M. Martins</p>
+                        <i class="fab fa-github"></i>
+                          <a href="https://github.com/myllenammartins"  target="_blank">GitHub</a> | <i class="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/myllenamirandamartins/" target="_blank">Linkedin</a>
+                    </section>
+            </footer>
   `;
 
   container.innerHTML = signInHTML;
